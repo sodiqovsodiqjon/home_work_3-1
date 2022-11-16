893ffb542915c22419e50399cad1380069e8f43e
sodiqovsodiqjon <sodiqjon191929@gmail.com>
2022-11-16 18:25:42 +0500

    coment push

---
+++ main.js
@@ -0,0 +1,27 @@
+let opt1 = document.querySelector(".texnolog")
+
+let text = document.querySelector(".text")
+
+let val = prompt("Alisherning yonida qancha so'm bor")
+
+let dollar = 500 + 250
+let evro = 120
+
+let dolUzb = 9433.34
+let erUzb = 10354.03
+
+let resUzb = dollar * dolUzb + evro * erUzb  
+
+
+console.log(opt1)
+
+if(text < 8317005) {
+    text.textContent = 'kechirasiz alisher ozroq sabir qling'
+}
+
+else{
+    text.textContent = 'Oq yo`l'
+}
+
+
+
